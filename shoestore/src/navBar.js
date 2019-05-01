d26927d6e3321eae48f73a451ee306bfb6aede8b
bengarcia1990 <garcbr18@gmail.com>
2019-05-01 10:07:35 -0400

    navBar CSS

--- shoestore/src/navBar.js
+++ shoestore/src/navBar.js
@@ -1,32 +1,37 @@
 import React from 'react';
+import './navBar.css';
 
 
 class Navbar extends React.Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="./App">Home</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
+            <nav className="navbar navbar-expand-lg">
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="./mens">Men's Shoes <span class="sr-only">(current)</span> </a>
+                        <li className="nav-item">
+                            <div className="nav-item-container">
+                                <a className="nav-link" href="./App">Home</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-item-container">
+                                <a className="nav-link" href="./mens">Men's Shoes</a>
+                            </div>
                         </li>
 
-                        <li className="nav-item active">
-                            <a className="nav-link" href="./womens">Women's Shoes <span class="sr-only">(current)</span></a>
+                        <li className="nav-item">
+                            <div className="nav-item-container">
+                                <a className="nav-link" href="./womens">Women's Shoes</a>
+                            </div>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <form className="form-inline">
+                        <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="searchBtn" type="submit">Search</button>
                     </form>
                 </div>
-            </nav>
+            </nav >
 
         )
     }
